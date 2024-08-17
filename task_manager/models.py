66d4cef89ca9d53ab0bc12f022278889502b7d62
Olena Zaritska <zaritskaos@gmail.com>
2024-08-17 17:44:24 +0300

    create models Task, TaskType, Worker & Position

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        choices=[
+            ("HIGH", "High"),
+            ("MEDIUM", "Medium"),
+            ("LOW", "Low"),
+        ],
+        max_length=255
+    )
+    task_type = models.ForeignKey(
+        "TaskType",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField("Worker", related_name="tasks")
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        "Position",
+        on_delete=models.CASCADE,
+        related_name="workers"
+    )
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
