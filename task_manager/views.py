2a999f4878a1fe29565e3b08be5683b91281ee3e
Olena Zaritska <zaritskaos@gmail.com>
2024-08-18 23:45:04 +0300

    add worker list view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+
+from task_manager.models import Worker
+
 
 def index(request):
     return render(request, "task_manager/index.html")
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    queryset = Worker.objects.select_related("position")
+    paginate_by = 4
