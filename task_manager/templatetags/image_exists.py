e5b1115dbf0f3fb591f30c39578d6b370a70c131
Olena Zaritska <zaritskaos@gmail.com>
2024-08-18 23:46:25 +0300

    add worker_list.html with image_exists template tag

---
+++ task_manager/templatetags/image_exists.py
@@ -0,0 +1,10 @@
+import os
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.filter
+def image_exists(username):
+    image_path = f"static/assets/img/{username}.jpg"
+    return os.path.exists(image_path)
