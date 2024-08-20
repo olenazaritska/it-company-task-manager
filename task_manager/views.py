550a6834916c1483244e3f236457235e5d6d0f3c
Olena Zaritska <zaritskaos@gmail.com>
2024-08-20 10:30:58 +0300

    add endpoint and view for worker detail

--- task_manager/views.py
+++ task_manager/views.py
@@ -12,3 +12,20 @@ class WorkerListView(generic.ListView):
     model = Worker
     queryset = Worker.objects.select_related("position")
     paginate_by = 4
+
+
+class WorkerDetailView(generic.DetailView):
+    model = Worker
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        worker = self.object
+        context['completed_tasks_count'] = worker.tasks.filter(is_completed=True).count()
+        context['pending_tasks_count'] = worker.tasks.filter(is_completed=False).count()
+
+        pending_tasks = worker.tasks.filter(is_completed=False)
+        for task in pending_tasks:
+            task.other_assignees = task.assignees.exclude(id=worker.id)
+        context['pending_tasks'] = pending_tasks
+
+        return context
