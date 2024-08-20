9155d2fe6c54e63bb899a1ebca94183b48d064de
Olena Zaritska <zaritskaos@gmail.com>
2024-08-20 12:35:21 +0300

    add crispy configs to settings

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'task_manager',
 ]
 
@@ -69,6 +71,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 WSGI_APPLICATION = 'it_company_task_manager.wsgi.application'
 
