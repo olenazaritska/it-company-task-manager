cb8410a3a75c7153a7957122dbb3dc73aa5ec418
Olena Zaritska <zaritskaos@gmail.com>
2024-08-22 20:53:35 +0300

    add auth endpoints

--- it_company_task_manager/urls.py
+++ it_company_task_manager/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
     path("", include("task_manager.urls", namespace="task-manager")),
 ]
