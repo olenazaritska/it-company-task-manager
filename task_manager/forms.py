7b3bc67d5f7330e64a1c218723ede536b76bcd14
Olena Zaritska <zaritskaos@gmail.com>
2024-09-04 11:31:51 +0300

    add task form

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.forms import fields
+from formset.widgets import DateTimeInput
 
-from task_manager.models import Worker
+from task_manager.models import Worker, Task
 
 
 class WorkerForm(UserCreationForm):
@@ -11,3 +15,17 @@ class WorkerForm(UserCreationForm):
             "last_name",
             "position",
         )
+
+
+class TaskForm(forms.ModelForm):
+    deadline = fields.DateTimeField(
+        widget=DateTimeInput,
+    )
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
